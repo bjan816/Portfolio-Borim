4dce8890d22cfa6b835a80945b9891914fd4ba85
bjan816 <bjan816@aucklanduni.ac.nz>
2023-11-17 15:41:47 +0900

    Navbar - add NavLinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
 import Link from 'next/link'
 import React from 'react'
+import NavLink from './NavLink'
+
+const navLinks = [
+    {
+        title: 'About',
+        path: '#about'
+    },
+    {
+        title: 'Projects',
+        path: '#projects'
+    },
+    {
+        title: 'Contact',
+        path: '#contact'
+    }
+]
 
 const Navbar = () => {
     return (
         <nav>
             <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-                <Link href={'/'} className='text-5xl text-white font-semibold'>LOGO</Link>
+                <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>LOGO</Link>
                 <div className='menu hidden md:block md:w-auto' id='navbar'>
-                    <ul>
-                        <li>
-                            <Link href={'#about'} className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'>About</Link>
-                        </li>
+                    <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+                        {
+                            navLinks.map((link, index) => (
+                                <li key={index}>
+                                    <NavLink href={link.path} title={link.title} />
+                                </li>
+                            ))
+                        }
                     </ul>
                 </div>
             </div>
