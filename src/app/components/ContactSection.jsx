58f674479ad033188a942b16d1593773473895ed
bjan816 <bjan816@aucklanduni.ac.nz>
2023-11-19 19:48:01 +0900

    Minor code format change

--- src/app/components/ContactSection.jsx
+++ src/app/components/ContactSection.jsx
@@ -6,7 +6,10 @@ import Image from 'next/image'
 
 const ContactSection = () => {
     return (
-        <section id='contact' className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
+        <section
+            id='contact'
+            className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'
+        >
             <div>
                 <h5 className='text-xl font-bold text-white my-2'>
                     Contact
