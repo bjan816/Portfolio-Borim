9a23a027729f55205789ab22fea9490520bfe4fe
bjan816 <bjan816@aucklanduni.ac.nz>
2023-11-19 12:44:43 +0900

    ProjectsSection - Add ProjectCard

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProjectsSection = () => {
+    return (
+        <div><h2>
+            My Projects
+        </h2>
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectsSection
