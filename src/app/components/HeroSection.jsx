1afb6963ac4ef67ad1487b840974ad74dfe13e90
bjan816 <bjan816@aucklanduni.ac.nz>
2023-11-23 17:00:07 +0900

    Add animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation'
 import { HiArrowNarrowRight } from 'react-icons/hi'
-import Link from "next/link"
 import { Link as ScrollLink } from 'react-scroll'
+import Link from "next/link"
+import { motion } from 'framer-motion'
 
 const HeroSection = () => {
     return (
         <section id='home' className='lg:py-16'>
             <div className='grid grid-cols-1 sm:grid-cols-12'>
-                <div className='col-span-8 place-self-center text-center sm:text-left justify-self-start mr-4'>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className='col-span-8 place-self-center text-center sm:text-left justify-self-start mr-4'>
                     <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-7xl sm:leading-normal lg:leading-normal font-extrabold'>
                         <span>
                             {/* className='text-transparent bg-clip-text bg-gradient-to-br from-purple-500 via-purple-500 to-purple-500' */}
@@ -68,9 +73,12 @@ const HeroSection = () => {
                             </span>
                             </Link>*/}
                     </div>
-                </div>
-                {/*<div className='col-span-4 place-self-center mt-4 lg:mt-0 ml-5'>
-                    <div className='rounded-full bg-[#181818] w-[250px] h-[250px] sm:w-[300px] sm:h-[300px] lg:w-[400px] lg:h-[400px] relative'>
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }} className='col-span-4 place-self-center mt-4 lg:mt-0 ml-5'>
+                    <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
                         <Image
                             src='/images/12.png'
                             alt='Hero image'
@@ -79,7 +87,7 @@ const HeroSection = () => {
                             height={300}
                         />
                     </div>
-                        </div>*/}
+                </motion.div>
             </div>
         </section>
 
