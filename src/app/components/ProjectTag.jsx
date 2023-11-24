1de86b2f2a5ec7d8a7721f473eddea6f365eb12c
bjan816 <bjan816@aucklanduni.ac.nz>
2023-11-24 16:54:37 +0900

    Minor updates

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -6,7 +6,8 @@ const ProjectTag = ({ name, onClick, isSelected }) => {
     : 'text-[#ADB7BE] border-slate-600 hover:border-gray-200 hover:text-gray-200';
     return (
         <button
-            className={`${buttonStyles} rounded-full border-4 px-6 py-3 text-xl cursor-pointer`}
+            className={`${buttonStyles} rounded-full px-6 py-3 text-xl cursor-pointer`}
+            style={{ borderWidth: '3px' }}
             onClick={() => onClick(name)}> {/*template literal*/}
             {name}
         </button>
