49f1bf83ba7ee35d88fbc6a1fa86775746f3179c
bjan816 <bjan816@aucklanduni.ac.nz>
2023-11-25 18:58:47 +0900

    Add gradient colour

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 const ProjectTag = ({ name, onClick, isSelected }) => {
     const buttonStyles = isSelected 
-    ? 'text-gray-200 border-purple-400' 
+    ? 'text-gray-100 btn border-gray-200' 
     : 'text-[#ADB7BE] border-slate-600 hover:border-gray-200 hover:text-gray-200';
     return (
         <button
-            className={`${buttonStyles} rounded-full px-6 py-3 text-xl cursor-pointer`}
-            style={{ borderWidth: '3px' }}
+            className={`${buttonStyles} border-2 rounded-full px-6 py-3 text-xl cursor-pointer`}
             onClick={() => onClick(name)}> {/*template literal*/}
             {name}
         </button>
