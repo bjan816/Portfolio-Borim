b486d33b98fc7d87e9b645ae17f50741a0cb8a22
bjan816 <bjan816@aucklanduni.ac.nz>
2024-11-30 03:11:53 +0900

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -30,7 +30,7 @@ const projectsData = [
         image: '/images/projects/Hopflix.png',
         tag: ['All', 'Web'],
         gitUrl: 'https://github.com/bjan816/Hopflix',
-        previewUrl: 'https://github.com/bjan816/Hopflix'
+        previewUrl: 'https://youtu.be/6TqB1HCR04k'
     },
     /*
     {
